ec131b3ee6ea8381d0640d69bdcdcd8f5b2c0d23
jamesLaurino <petersthomas90@gmail.com>
2025-10-18 10:48:09 +0200

    persistance + system message

--- src/main/java/com/trackcreation/budgettrack/controller/AIAgentController.java
+++ src/main/java/com/trackcreation/budgettrack/controller/AIAgentController.java
@@ -4,14 +4,20 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AIAgentController {
 
     private ChatClient chatClient;
 
+    // simple persistence
     public AIAgentController(ChatClient.Builder builder, ChatMemory memory) {
         this.chatClient = builder
                 .defaultAdvisors(new SimpleLoggerAdvisor())
@@ -20,10 +26,23 @@ public class AIAgentController {
                 .build();
     }
 
-    // Simple asking => no persistence
+    // Simple asking
     @GetMapping("/chat")
     public String askLLM(String query) {
         return chatClient.prompt()
+                .user(query).call().content();
+    }
+
+    // Asking with message system
+    @GetMapping("/chat/system")
+    public String askLLMSystemMessage(String query) {
+        List<Message> exemple = List.of(
+                new UserMessage("6+4"),
+                new AssistantMessage("Le résultat est : 10")
+        );
+        return chatClient.prompt()
+                .system("Répond toujours en majuscule")
+                .messages(exemple)
                 .user(query).call().content();
     }
 
